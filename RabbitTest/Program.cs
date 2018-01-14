7a50755fb7edf218e0338b86dd16701256154f0c
Aaron Cure <aaron.cure@cypressdefense.com>
2018-01-14 08:27:50 -0700

    Fixed RabbitTest

--- RabbitTest/Program.cs
+++ RabbitTest/Program.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,30 +23,3 @@ namespace RabbitTest
         }
     }
 }
-=======
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.EntityFrameworkCore;
-
-using Puma.Prey.Rabbit.EF;
-using Puma.Prey.Rabbit.Models;
-
-namespace RabbitTest
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            RabbitDBContext context = new RabbitDBContext();
-            if (!context.AllMigrationsApplied())
-                context.Database.Migrate();
-            context.EnsureSeedData();
-
-            var test = context.Hunts.Include(h => h.Animals).FirstOrDefault();
-            Console.WriteLine(test.PhoneNumber);
-            Console.WriteLine(test.Animals.Count());
-        }
-    }
-}
->>>>>>> 0244145489d1a5a74921927ccc798ecb16b27460
