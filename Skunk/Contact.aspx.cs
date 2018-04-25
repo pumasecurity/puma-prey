93d2e51698d7b301add715695e94f168bb3a6657
ejohn20 <eric.johnson@cypressdefense.com>
2018-04-25 12:38:57 -0500

    Added standard DLL to help with CI scanning. Contact file download issue.

--- Skunk/Contact.aspx.cs
+++ Skunk/Contact.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +12,16 @@ namespace Skunk
 	{
 		protected void Page_Load(object sender, EventArgs e)
 		{
+            string fileName = Request["fileName"];
 
+            using (Stream stream2 = new FileStream(fileName, FileMode.Open))
+            {
+                //Do some stuff
+            }
+
+            FileStream stream = File.Open(fileName, FileMode.Open);
+
+            Response.WriteFile(fileName);
 		}
 	}
 }
