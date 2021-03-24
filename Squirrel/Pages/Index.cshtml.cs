c11dd6bfd66c3922d3ce127288fa5f394ef73877
Eric M. Mead <eric.mead@pumascan.com>
2021-03-24 13:35:18 -0500

    stub for a core 3.1 web project

---
+++ Squirrel/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Squirrel.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            System.Random random = new Random();
+        }
+    }
+}
