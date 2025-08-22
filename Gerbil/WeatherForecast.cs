3ea56be1ad739dd2fb93f3021d3ca6454f9769e1
Eric M Mead <eric.mead@pumasecurity.io>
2025-08-21 19:52:31 -0500

    add a result

--- Gerbil/WeatherForecast.cs
+++ Gerbil/WeatherForecast.cs
@@ -9,5 +9,10 @@ namespace Gerbil
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+
+        public WeatherForecast()
+        {
+            TemperatureC = new System.Random().Next();
+        }
     }
 }
